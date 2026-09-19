1144ed293dd77c275396b1a3879db399eb419ade
Melvin Mathew <melvinmv02@gmail.com>
2026-09-19 12:44:31 +0530

    Update ButtonClickExample.java
    Removed commented code explaining ActionEvent and event handlers.

--- ButtonClickExample.java
+++ ButtonClickExample.java
@@ -18,8 +18,8 @@ public class ButtonClickExample extends Application {
         button.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                //handle is the specific name that JavaFX requires for event handlers.
-                // ActionEvent event is the parameter that JavaFX automatically passes to              the method when the button is clicked.
+                
+    
 // ActionEvent is an object that contains information about the click .
                 label.setText("Button Clicked!");
             }
@@ -28,7 +28,7 @@ public class ButtonClickExample extends Application {
 
 
         // Arrange controls vertically
-        VBox root = new VBox(10); // spacing = 10 pixels
+        VBox root = new VBox(10); 
         root.getChildren().addAll(label, button);
 
         Scene scene = new Scene(root, 250, 150);
