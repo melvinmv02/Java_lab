b26bed2f95eb009221a173a43a04bc9e23352c32
Melvin Mathew <melvinmv02@gmail.com>
2026-09-19 12:43:07 +0530

    Update BusTicketBooking.java

--- BusTicketBooking.java
+++ BusTicketBooking.java
@@ -1,10 +1,10 @@
-// Concept: static Fields, static Methods, final Constants, Instance Fields and User Input
+
 
 import java.util.Scanner;
 
 class Ticket {
 
-    // Instance field
+    
     String passengerName;
 
     // Static field - shared by all Ticket objects
@@ -13,12 +13,12 @@ class Ticket {
     // Static final constant - fixed value
     static final double BASE_FARE = 50.0;
 
-    // Constructor
+
     Ticket(String passengerName) {
 
         this.passengerName = passengerName;
 
-        // Increment total ticket count
+        
         ticketCount++;
 
         System.out.println(
@@ -27,7 +27,7 @@ class Ticket {
         );
     }
 
-    // Static method
+    
     static void printTotalTicketsSold() {
 
         System.out.println(
@@ -42,7 +42,6 @@ public class BusTicketBooking {
 
         Scanner sc = new Scanner(System.in);
 
-        // Taking passenger details from the user
         System.out.print("Enter passenger 1 name: ");
         String name1 = sc.nextLine();
 
@@ -52,22 +51,17 @@ public class BusTicketBooking {
         System.out.print("Enter passenger 3 name: ");
         String name3 = sc.nextLine();
 
-        // Creating three Ticket objects
+       
         Ticket ticket1 = new Ticket(name1);
         Ticket ticket2 = new Ticket(name2);
         Ticket ticket3 = new Ticket(name3);
 
-        // Calling static method using class name
+       
         Ticket.printTotalTicketsSold();
 
-        /*
-        ERROR:
+     
 
-        Ticket.BASE_FARE = 100.0;
-
-        BASE_FARE is declared as final,
-        so its value cannot be changed.
-        */
+     
 
         System.out.println(
                 "Base Fare: Rs. " + Ticket.BASE_FARE
